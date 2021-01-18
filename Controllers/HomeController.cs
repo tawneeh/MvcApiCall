d3f44dde0e2555debd5708495153ccb24577eb1a
Tawnee Harris <tawneeh@icloud.com>
2021-01-18 14:40:29 -0800

    lesson 17 and 18

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MvcApiCall.Models;
 
 namespace MvcApiCall.Controllers
@@ -12,18 +7,8 @@ namespace MvcApiCall.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var allArticles = Article.GetArticles(EnvironmentVariables.ApiKey);
+            return View(allArticles);
         }
     }
 }
---
+++ Models/EnvironmentVariables.cs
@@ -0,0 +1,7 @@
+namespace MvcApiCall.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string ApiKey = "MCG1weiYSgBg4lezXXHACPFm0NVoJZEf";
+    }
+}
